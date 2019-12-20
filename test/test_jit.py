0a2dae72f420f463981e829409dd3156f49e2b83
eellison <eellison@fb.com>
2019-12-20 15:25:55 -0800

    add float[] str[] constants
    ghstack-source-id: 6528534fec766f514edbf92e6b8d10e5788d708c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31503

--- test/test_jit.py
+++ test/test_jit.py
@@ -3153,6 +3153,28 @@ graph(%Ra, %Rb):
         self.run_pass('constant_propagation', graph)
         self.assertTrue(graph.findNode("prim::Loop").outputsSize() == 2)
 
+    def test_constant_insertion(self):
+        def foo():
+            a = [1.0, 2.0, 3.0]
+            b = ["ab", "cd", "ef"]
+            return a, b
+
+        scripted_foo = torch.jit.script(foo)
+        graph = scripted_foo.graph
+        FileCheck().check_count("ListConstruct", 2).run(graph)
+        self.run_pass('constant_propagation', graph)
+        FileCheck().check_not("ListConstruct").run(graph)
+        FileCheck().check_dag("float[] =").check_dag("str[] =").run(graph)
+        imported = self.getExportImportCopy(scripted_foo)
+        self.assertEqual(foo(), scripted_foo())
+        self.assertEqual(imported(), scripted_foo())
+
+        @torch.jit.script
+        def test_empty():
+            return torch.jit.annotate(List[str], [])
+        imported = self.getExportImportCopy(test_empty)
+        FileCheck().check("str[]").run(imported.graph)
+
     def test_trace_detach(self):
         def foo(x, w):
             return torch.matmul(x, w).detach()
